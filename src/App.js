673a5f383c4e8f56f044c1d5d8c7fbf6d978e85c
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-09-07 10:06:41 -0500

    add M-ui styles and simple layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+//material-ui
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+//
+import NotesForm from './NotesForm'
 
 function App() {
   return (
-    <h1>Hello world</h1>
+     <Fragment>
+      <Typography align="center" variant="h2" gutterBottom>
+        Hello World
+      </Typography>
+      <Grid container justify='center' spacing={2}>
+        <Grid item xs={4}>
+        </Grid>
+        <Grid item xs={8}>
+          <NotesForm/>
+        </Grid>
+      </Grid>
+      <Fab color="primary" className='addIcon'>
+        <AddIcon />
+      </Fab>
+     </Fragment>
   );
 }
 
